0920cdbb3a1b49e3aa2065629264871c6b2f3c21
Chaofanman <gchase.patron@gmail.com>
2016-08-14 12:32:39 +0800

    aww yeah

--- src/Calculator.js
+++ src/Calculator.js
@@ -16,10 +16,22 @@ const inputButtons = [
 ];
 
 class Calculator extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            inputValue: 0,
+            previousInputValue: 0,
+            operator: null
+        }
+    }
+
     render() {
         return (
             <View style={Style.rootContainer}>
-                <View style={Style.displayContainer}></View>
+                <View style={Style.displayContainer}>
+                    <Text style={Style.displayText}>{this.state.inputValue}</Text>
+                </View>
                 <View style={Style.inputContainer}> 
                     {this._renderInputButtons()}
                 </View>
@@ -41,17 +53,62 @@ class Calculator extends Component {
             for(var i = 0; i < row.length; i++){
                 //goes through each value in the sub arrays of inputButtons
                 let input = row[i];
+                console.log(i);
                 //puts each value in each InputButton
                 inputRow.push(
                     //props value
-                    <InputButton value={input} />
+                    <InputButton value={input} highlight={this.state.operator === input} onPress={this._onInputButtonPressed.bind(this, input)}/>
                 );
             }
             //puts all inputRows in views
             views.push(<View style={Style.inputRow}>{inputRow}</View>);
         }
-        
+
         return views;
+    }
+
+    _onInputButtonPressed(input){
+        switch(typeof input){
+            case 'number':
+                return this._handleInputNumber(input);
+            case 'string':
+                return this._handleInputString(input);
+        }
+    }
+
+    _handleInputNumber(num){
+        let inputValue = (this.state.inputValue * 10) + num;
+        this.setState({inputValue});
+    }
+
+    _handleInputString(str){
+        switch(str){
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+                this.setState({
+                    operator: str,
+                    previousInputValue: this.state.inputValue,
+                    inputValue: 0 
+                });
+                break;
+            case '=':
+                let symbol = this.state.operator,
+                    inputValue = this.state.inputValue,
+                    previousInputValue = this.state.previousInputValue;
+                
+                if (!symbol){
+                    return;
+                }
+
+                this.setState({
+                    previousInputValue: 0,
+                    inputValue: eval(previousInputValue + symbol + inputValue),
+                    operator: null
+                });
+                break;
+        }
     }
 }
 
